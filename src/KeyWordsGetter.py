900d2cd9f9b3386ebdda753ef17267d9d7b54ea0
Peter <54754814+Potriashka@users.noreply.github.com>
2021-08-31 01:27:23 +0300

    Create KeyWordsGetter.py

---
+++ src/KeyWordsGetter.py
@@ -0,0 +1,15 @@
+# Key Word(s) - KW
+
+# creating an empty list
+lst = []
+ 
+# number of KW in input
+n = int(input("Enter number of key words : "))
+ 
+# iterating till the range
+for i in range(0, n):
+    ele = int(input())
+ 
+    lst.append(ele) # adding the KW
+     
+print(lst)
